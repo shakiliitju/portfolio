484e61851f258a9eb6584fbd59a6a0a224429b45
Md. Shakil Hossain <shakiljusc15@gmail.com>
2022-02-10 22:58:03 +0600

    traveling update

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student" , "Programmer", "Web Developer", "Blogger", "Graphic Designer",  "Ethical Hacker"],
+        strings: ["Student" , "Programmer", "Web Developer", "Blogger", "Graphic Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student" , "Programmer", "Web Developer", "Blogger", "Graphic Designer",  "Ethical Hacker"],
+        strings: ["Student" , "Programmer", "Web Developer", "Blogger", "Graphic Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
